e321311a04c994c8c1999bd2bd778788c3c9994f
raphaelsoul <raphaelsoul@163.com>
2015-09-01 00:04:03 +0900

    Signed-off-by: raphaelsoul <raphaelsoul@163.com>

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,11 +6,11 @@ $(document).ready(function(){
     $("#sidebar").hover(function(){
         //$("#sidebar").css("left","0px");
         $("#sidebar").animate({left:'0px'});
-        $("#PostArea").css({"transform":"rotateY(40deg)","transition":"transform 1s ease 0s"})
+        $("#PostArea").css({"transform":"rotateY(45deg) perspective(600px)","transition":"transform 1s ease 0s"});
     },function(){
         //$("#sidebar").css("left","-160px");
-        $("#sidebar").animate({left:'-160px'});
-        $("#PostArea").css({"transform":"rotateY(0deg)","transition":"transform 1s ease 0s"})
+        $("#sidebar").animate({left:'-260px'});
+        $("#PostArea").css({"transform":"rotateY(0deg) perspective(0px)","transition":"transform 1s ease 0s"});
         //,"transform-origin":"100% 50% 0%"
     });
 });
